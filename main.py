bd82a8d70e77e479326d34a0debc1ae5a4b60c88
--global <sendmailtochanna@gmail.com>
2019-04-29 20:39:51 -0700

    add updates to prototype
    	- user uploads student_name, subject_name, assignment file (image)
    	- application converts image to text
    	- then upload key value pair (subject_name, student_name) --> converted_text to google datastore

--- main.py
+++ main.py
@@ -9,34 +9,51 @@ from flask import url_for
 from joblib import load
 from werkzeug.utils import secure_filename
 from plagiarism_logic import PlagiarismCheck
+import cgi, cgitb, jinja2
+from google.cloud import datastore
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "plagiarism-check-image-docs-596999b61703.json"
 
-UPLOAD_FOLDER = '/Users/channa/Projects/plagiarism-check-on-image-docs/uploaded_imgs'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+# UPLOAD_FOLDER = '/Users/channa/Projects/plagiarism-check-on-image-docs/uploaded_imgs'
+# ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+datastore_client = datastore.Client()
+
+
+def get_entity(student_name, student_subject, student_submission):
+	print(student_name, student_subject, student_submission)
+	task_key = datastore_client.key(student_subject, student_name)
+	task = datastore.Entity(key=task_key)
+	task['description'] = student_submission
+	return task
 
 @app.route('/uploader', methods = ['POST'])
 def upload_file():
+
 	if request.method == 'POST':
-		# check if the post request has the file part
-		if 'file' not in request.files:
-			print('No file part')
+
+		if 'subject-name' not in request.form:
+			print('No subject name')
 			return redirect(request.url)
-		file = request.files['file']
-		# if user does not select file, browser also
-		# submit a empty part without filename
-		if file.filename == '':
-			print('No selected file')
+		subject_name = request.form['subject-name']
+
+		if 'student-name' not in request.form:
+			print('No student name')
 			return redirect(request.url)
-		if file:
-			filename = secure_filename(file.filename)
-			upload_folder = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-			file.save(upload_folder)
+		student_name = request.form['student-name']
+
+		if 'assignment-file' not in request.form:
+			print('No assignment file')
+			return redirect(request.url)
+		assignment_file = request.form['assignment-file']
 
 		obj = PlagiarismCheck()
-		img_text = obj.get_text(upload_folder)
+		img_text = obj.get_text(assignment_file)
+
+		datastore_client.put(get_entity(str(student_name), str(subject_name), str(img_text)))
 
 	return jsonify(return_value = img_text)
 
