2fdfd9efdcba300b4a4e4c78a821744a09e22aa2
abhijithshreesh <ashreesh@asu.edu>
2019-04-30 21:52:01 -0700

    add save file capability

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "plagiarism-check-image-docs-5969
 
 app = Flask(__name__)
 # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.config['UPLOAD_FOLDER'] = os.path.dirname(os.path.abspath(__file__)) + '/temp'
 datastore_client = datastore.Client()
 
 
@@ -30,6 +30,8 @@ def get_entity(student_name, student_subject, student_submission):
 	task = datastore.Entity(key=task_key)
 	task['description'] = student_submission
 	return task
+
+
 
 @app.route('/uploader', methods = ['POST'])
 def upload_file():
@@ -46,15 +48,25 @@ def upload_file():
 			return redirect(request.url)
 		student_name = request.form['student-name']
 
-		if 'assignment-file' not in request.form:
-			print('No assignment file')
+		# check if the post request has the file part
+		if 'assignment-file' not in request.files:
+			print('No file part')
 			return redirect(request.url)
-		assignment_file = request.form['assignment-file']
+		file = request.files['assignment-file']
+		# if user does not select file, browser also
+		# submit a empty part without filename
+		if file.filename == '':
+			print('No selected file')
+			return redirect(request.url)
+		if file:
+			filename = secure_filename(file.filename)
+			upload_folder = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+			file.save(upload_folder)
 
 		obj = PlagiarismCheck()
-		img_text = obj.get_text(assignment_file)
+		img_text = obj.get_text(upload_folder)
 
-		datastore_client.put(get_entity(str(student_name), str(subject_name), str(img_text)))
+		datastore_client.put(get_entity(str(student_name), str(subject_name), str(img_text.encode('ascii', 'ignore'))))
 
 	return jsonify(return_value = img_text)
 
